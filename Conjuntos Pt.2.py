48b80ce77b10013fdd99adae02ca765069ad874b
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-27 20:57:39 -0600

    primera version

---
+++ Conjuntos Pt.2.py
@@ -0,0 +1,17 @@
+#Conjuntos parte 2 -> matemáticamente
+
+a = {1,2,3}
+b = {3,4,5}
+
+#Unión de conjuntos
+c = a | b
+
+#Intersección
+c = a & b
+
+#Diferencia
+c = a - b
+
+print(c)
+
+#Carolina EM
